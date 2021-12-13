104429f6394287b00abba2b66025ec1689095d7e
Erik Jacobsen <eljacobsen12@gmail.com>
2021-12-12 23:55:34 -0500

    backend_files
    files for backend processing of collections.

---
+++ ibb_upload.py
@@ -0,0 +1,20 @@
+"""
+Upload Image to ibb.co through API.
+
+@author: EJacobsen
+@date: 12/12/2021
+
+"""
+
+import requests
+import base64
+
+PATH = f'C:\\Users\\ejacobsen\\Downloads\\EJacobsen\\crypto_tools\\Convex Labs\\honestnft-shenanigans\\fair_drop\\scatterplots\\0N1_Force_scatterplot.png'
+with open(PATH, "rb") as img_file:
+    myString = base64.b64encode(img_file.read())
+
+ibbUrl = 'https://api.imgbb.com/1/upload?expiration=600&key=bae55b4c4ae5fadca0c1d517a1e25069'
+
+myObj = {'image':myString}
+x = requests.post(ibbUrl, data = myObj)
+print(x._content)
